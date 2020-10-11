eb5dc89ddadbd03c7c59d402788b9c077bf0b7b1
anastasia <mrs.anastasia21@gmail.com>
2020-10-11 20:14:26 +0300

    Add HW2

---
+++ HW/HomeWork2.php
@@ -0,0 +1,43 @@
+<?php
+
+$task_manager = [
+    [
+        'ID' => 1998,
+        'title' => 'Indexation',
+        'description' =>  'Add all URLs of Site1 and Site2 to GSC',
+        'owner' => 'Anna',
+        'deadline' => '02.10.2020',
+        'status' => 'Done',
+    ],
+    [
+        'ID' => 2003,
+        'title' => 'Audit',
+        'description' => 'Use Screaming Frog for Audit',
+        'owner' => 'Tanya',
+        'deadline' => '14.10.2020',
+        'status' => 'In process',
+    ],
+    [   'ID' => 2012,
+        'title' => 'Semantic Core',
+        'description' => 'Make SC for Site1 and Site 2',
+        'owner' => 'Oleg',
+        'deadline' => '04.10.2020',
+        'status' => 'Done',
+    ],
+    [   'ID' => 2015,
+        'title' => 'Linkbuilding',
+        'description' => 'Buy links for Site1 and Site2',
+        'owner' => 'Denis',
+        'deadline' => '30.10.2020',
+        'status' => 'In Process',
+    ],
+    [   'ID' => 2018,
+        'title' => 'Meta Tags',
+        'description' => 'Add Title and Description for Site3 and Site4',
+        'owner' => 'Maria',
+        'deadline' => '09.10.2020',
+        'status' => 'Done',
+    ],
+];
+var_dump($task_manager);
+
